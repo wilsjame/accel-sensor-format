e2125836bf7b3756f8b774bd68394775e5e8e12a
James Wilson <jgwilson1214@gmail.com>
2019-11-10 19:09:30 -0800

    glink: trim and change out file extension
    trim out file extension and change to _FORMATTED.txt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // start up
+    // G-Link-200
+    connect(ui->lineEdit_glink_file_path, SIGNAL(textChange(QString)), this, SLOT(on_lineEdit_glink_file_path_textChanged()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_lineEdit_glink_file_path_textChanged()
+{
+    glink_file = ui->lineEdit_glink_file_path->text();
+
+    // change file extension to .txt
+    size_t lastIndex = glink_file.toStdString().find_last_of(".");
+    std::string rawName = glink_file.toStdString().substr(0, lastIndex);
+    glink_out_file = rawName.append("_FORMATTED.txt");
+
+    // display out file preview
+    ui->label_glink_format_file_path->setText(QString::fromStdString(glink_out_file));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_lineEdit_glink_file_path_textChanged();
+
 private:
     Ui::MainWindow *ui;
+
+    // G-Link-200
+    QString glink_file;
+    std::string glink_out_file;
+
+    // nPoint
 };
 #endif // MAINWINDOW_H
