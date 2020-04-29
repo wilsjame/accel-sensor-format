8531c7fa02b173f66d543efc2267d60461f9be43
James Wilson <jgwilson1214@gmail.com>
2020-04-29 12:44:08 -0700

    remove biostamp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,8 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // *** G-Link ***
     // start up
-    // G-Link
     connect(ui->lineEdit_glink_file_path, SIGNAL(textChange(QString)), this, SLOT(on_lineEdit_glink_file_path_textChanged()));
 }
 
@@ -23,13 +23,17 @@ MainWindow::~MainWindow()
 }
 
 // *** G-Link ***
-// use file explorer to find file to format
+//
+// Use file explorer to find in file to format. Default directory is user's home path.
+//
 void MainWindow::on_pushButton_glink_file_path_clicked()
 {
     ui->lineEdit_glink_file_path->setText(QFileDialog::getOpenFileName(this, "Select G-Link file to format", QDir::homePath()));
 }
 
-// trim out file extension and display path preview
+//
+// Trim in file extension and display out file path preview.
+//
 void MainWindow::on_lineEdit_glink_file_path_textChanged()
 {
     glink_file = ui->lineEdit_glink_file_path->text();
@@ -43,15 +47,21 @@ void MainWindow::on_lineEdit_glink_file_path_textChanged()
     ui->label_glink_format_file_path->setText(QString::fromStdString(glink_out_file));
 }
 
-// format glink data into tab delimited acceleration data
+//
+// Format glink data into tab delimited acceleration data and write to out file.
+//
 void MainWindow::on_pushButton_glink_format_clicked()
 {
+    if (glink_file == "") {
+        return;
+    }
+
     std::ofstream outFile(glink_out_file);
     std::ifstream inFile(glink_file.toStdString());
     std::string line, value, temp;
     std::vector<std::string> row;
 
-    // parse header
+    // parse file header ignore all lines before data
     while (line != "DATA_START")
     {
         std::getline(inFile, line);
@@ -62,7 +72,6 @@ void MainWindow::on_pushButton_glink_format_clicked()
     // parse data
     std::stringstream ss;
 
-    //TODO verify tabs
     while (std::getline(inFile, line))
     {
         row.clear();
@@ -80,63 +89,19 @@ void MainWindow::on_pushButton_glink_format_clicked()
 
             if (next(i) != row.end())
             {
-                //std::cout << *i << '\t';
+                //std::cerr << *i << '\t';
                 outFile << *i << '\t' << std::flush;
             }
             else
             {
-                //std::cout << *i;
+                //std::cerr << *i;
                 outFile << *i << std::flush;
             }
 
         }
 
-        //std::cout << std::endl;
+        //std::cerr << std::endl;
         outFile << std::endl;
     }
-
-}
-
-// *** BioStamp ***
-
-// use file explorer to find files to format and combine
-void MainWindow::on_pushButton_biostamp_left_arm_file_path_clicked()
-{
-    ui->lineEdit_biostamp_left_arm_file_path->setText(QFileDialog::getOpenFileName(this, "Select BioStamp left arm accel.csv", QDir::homePath()));
-}
-
-void MainWindow::on_pushButton_biostamp_right_arm_file_path_clicked()
-{
-    ui->lineEdit_biostamp_right_arm_file_path->setText(QFileDialog::getOpenFileName(this, "Select BioStamp right arm accel.csv", QDir::homePath()));
-}
-
-void MainWindow::on_pushButton_biostamp_torso_file_path_clicked()
-{
-    ui->lineEdit_biostamp_torso_file_path->setText(QFileDialog::getOpenFileName(this, "Select BioStamp torso accel.csv", QDir::homePath()));
-}
-
-// get the file names for left arm, right arm, and torso data sets
-void MainWindow::on_lineEdit_biostamp_left_arm_file_path_textChanged(const QString &arg1)
-{
-    biostamp_file_left_arm = arg1;
-}
-
-void MainWindow::on_lineEdit_biostamp_right_arm_file_path_textChanged(const QString &arg1)
-{
-    biostamp_file_right_arm = arg1;
-}
-
-void MainWindow::on_lineEdit_biostamp_torso_file_path_textChanged(const QString &arg1)
-{
-    biostamp_file_torso = arg1;
-}
-
-// format
-void MainWindow::on_pushButton_biostamp_format_clicked()
-{
-
-    //TODO check left arm, right arm, and torso file paths are populated
-
-    Samples samples;
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,53 +16,18 @@ public:
     ~MainWindow();
 
 private slots:
+    // *** G-Link ***
     void on_lineEdit_glink_file_path_textChanged();
 
     void on_pushButton_glink_file_path_clicked();
 
     void on_pushButton_glink_format_clicked();
 
-    void on_pushButton_biostamp_right_arm_file_path_clicked();
-
-    void on_pushButton_biostamp_left_arm_file_path_clicked();
-
-    void on_pushButton_biostamp_torso_file_path_clicked();
-
-    void on_pushButton_biostamp_format_clicked();
-
-    void on_lineEdit_biostamp_left_arm_file_path_textChanged(const QString &arg1);
-
-    void on_lineEdit_biostamp_right_arm_file_path_textChanged(const QString &arg1);
-
-    void on_lineEdit_biostamp_torso_file_path_textChanged(const QString &arg1);
-
 private:
     Ui::MainWindow *ui;
 
-    // G-Link
+    // *** G-Link ***
     QString glink_file;
     std::string glink_out_file;
-
-    // BioStamp
-    QString biostamp_file_left_arm;
-    QString biostamp_file_right_arm;
-    QString biostamp_file_torso;
-    std::string biostamp_out_file;
-
-    struct Sample
-    {
-        float left_x;
-        float left_y;
-        float left_z;
-        float right_x;
-        float right_y;
-        float right_z;
-        float torso_x;
-        float torso_y;
-        float torso_z;
-    };
-
-    typedef std::vector<Sample> Samples;
-
 };
 #endif // MAINWINDOW_H
