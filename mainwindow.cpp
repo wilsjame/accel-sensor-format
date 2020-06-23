34b767b3383837d8c04f22ba7ca6848af79d9139
James Wilson <jgwilson1214@gmail.com>
2020-06-22 23:34:25 -0700

    trim all headers before time and data

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,15 +41,59 @@ void MainWindow::on_lineEdit_glink_file_path_textChanged()
     // change file extension to .txt
     size_t lastIndex = glink_file.toStdString().find_last_of(".");
     std::string rawName = glink_file.toStdString().substr(0, lastIndex);
-    glink_out_file = rawName.append("_FORMATTED.txt");
+    glink_out_file = rawName.append("_FORMATTED.csv");
 
     // display out file preview
     ui->label_glink_format_file_path->setText(QString::fromStdString(glink_out_file));
 }
 
+
+// The older glink sensor output had one time header.
+// glinkcombine.vi checks the first 4 characters of each header
+// and expects only one header to start with "Time".
+// The newer glink sensor output has multitple headers that
+// begin with "Time" before the data actually starts.
+// This trims all the headers before the true time header
+// so glinkcombine.vi can take the file as input as if it
+// was an older glink sensor output.
+void MainWindow::on_pushButton_glink_format_clicked()
+{
+     if (glink_file == "") {
+        return;
+    }
+
+    std::ofstream outFile(glink_out_file);
+    std::ifstream inFile(glink_file.toStdString());
+    std::string line;
+
+    // parse file header ignore all lines before data
+    while (line != "DATA_START")
+    {
+        std::getline(inFile, line);
+    }
+
+    for (int i=0; i<1000; i++)
+    {
+        std::getline(inFile, line);
+        outFile << line;
+        outFile << std::endl;
+    }
+
+    /*
+    while (std::getline(inFile, line))
+    {
+        outFile << line;
+    }
+    */
+    outFile << std::endl;
+
+}
+
+// STALE: assumes input data is 3 glink sensor results outputted into one file. ex) xyz xyz xyz
 //
 // Format glink data into tab delimited acceleration data and write to out file.
 //
+/*
 void MainWindow::on_pushButton_glink_format_clicked()
 {
     if (glink_file == "") {
@@ -105,3 +149,4 @@ void MainWindow::on_pushButton_glink_format_clicked()
     }
 
 }
+*/
